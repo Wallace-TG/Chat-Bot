2fe285854bb5e5c072a85cfcd6943f05929b9cdc
Wallace-TG <wallacetg698@gmail.com>
2025-05-01 16:53:37 -0300

    Add files via upload

--- chatbot.js
+++ chatbot.js
@@ -28,11 +28,8 @@ client.on('message', async msg => {
         await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
         const contact = await msg.getContact(); //Pegando o contato
         const name = contact.pushname; //Pegando o nome do contato
-        await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + 'Sou o assistente virtual da empresa tal. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas'); //Primeira mensagem de texto
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(5000); //Delay de 5 segundos
-    
+        await client.sendMessage(msg.from,'Olá! '+ name.split(" ")[0] + 'Sou o assistente virtual da empresa SoftForce. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona, Valores dos planos\n2 - Agendar visita técnica \n3 - Problemas com a internet\n4 - Desejo falar com um atendente\n5 - Outro duvida'); //Primeira mensagem de texto
+      
         
     }
 
@@ -46,7 +43,7 @@ client.on('message', async msg => {
         await delay(3000); //delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, 'Nosso serviço oferece consultas médicas 24 horas por dia, 7 dias por semana, diretamente pelo WhatsApp.\n\nNão há carência, o que significa que você pode começar a usar nossos serviços imediatamente após a adesão.\n\nOferecemos atendimento médico ilimitado, receitas\n\nAlém disso, temos uma ampla gama de benefícios, incluindo acesso a cursos gratuitos');
+        await client.sendMessage(msg.from, 'Nosso serviço oferecemos planos de internet com preços acessíveis e atendimento 24 horas por dia.\n\n*Planos disponíveis:*\n1 - Basico:\n300MB por  R$100/Mês\n2 - Intermediário:\n500MB por R$120/Mês\n3 - Premium:\n700MB por R$160/Mês \n\n*Benefícios:*\n- Atendimento 24 horas por dia\n- Suporte técnico especializado\n- Internet de alta velocidade\n- Sem taxa de instalação\n- Sem fidelidade\n- Sem burocracia');
 
         await delay(3000); //delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
@@ -56,7 +53,7 @@ client.on('message', async msg => {
         await delay(3000); //delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+        await client.sendMessage(msg.from, 'Link para cadastro: https://SoftForce/cadastro.com');
 
 
     }
@@ -68,12 +65,12 @@ client.on('message', async msg => {
         await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, '*Plano Individual:* R$22,50 por mês.\n\n*Plano Família:* R$39,90 por mês, inclui você mais 3 dependentes.\n\n*Plano TOP Individual:* R$42,50 por mês, com benefícios adicionais como\n\n*Plano TOP Família:* R$79,90 por mês, inclui você mais 3 dependentes');
+        await client.sendMessage(msg.from, 'Agendar visita técnica é muito simples.\n\n1º Passo\nEscolha o dia e horário que deseja agendar a visita técnica.\n\n2º Passo\nApós escolher o dia e horário, você receberá uma confirmação de agendamento no seu email cadastrado.\n\n3º Passo\nAguarde a visita técnica no dia e horário agendado.');
 
         await delay(3000); //delay de 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+        await client.sendMessage(msg.from, 'Link para agendar sua visita: https://SoftForce/vtecnica.com');
     }
 
     if (msg.body !== null && msg.body === '3' && msg.from.endsWith('@c.us')) {
@@ -83,12 +80,8 @@ client.on('message', async msg => {
         await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, 'Sorteio de em prêmios todo ano.\n\nAtendimento médico ilimitado 24h por dia.\n\nReceitas de medicamentos');
+        await client.sendMessage(msg.from, 'Se você está enfrentando problemas com a internet, siga os passos abaixo:\n\n1º Passo\nVerifique se o seu modem está ligado e conectado corretamente.\n\n2º Passo\nReinicie o modem e aguarde alguns minutos.\n\n3º Passo\nCaso o problema persista, entre em contato com o nosso suporte técnico pelo WhatsApp ou pelo telefone.');
         
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
 
     }
 
@@ -98,15 +91,14 @@ client.on('message', async msg => {
         await delay(3000); //Delay de 3000 milisegundos mais conhecido como 3 segundos
         await chat.sendStateTyping(); // Simulando Digitação
         await delay(3000);
-        await client.sendMessage(msg.from, 'Você pode aderir aos nossos planos diretamente pelo nosso site ou pelo WhatsApp.\n\nApós a adesão, você terá acesso imediato');
-
-
-        await delay(3000); //delay de 3 segundos
-        await chat.sendStateTyping(); // Simulando Digitação
-        await delay(3000);
-        await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
-
-
+        await client.sendMessage(msg.from, 'Se você deseja falar com um atendente, siga os passos abaixo:\n\n1º Passo\nDigite o número do seu protocolo de atendimento.\nCaso você tenha um número ignore esse passo\n2º Passo\nAguarde a confirmação do atendente.\n\n3º Passo\nO atendente irá entrar em contato com você pelo WhatsApp ou pelo telefone.');
+       
+        // Novo passo: Verifica se nenhuma das etapas resolveu
+        await delay(5000); // Espera um pouco antes de continuar
+        await chat.sendStateTyping()
+        await delay(2000);
+        await client.sendMessage(msg.from, 'Se mesmo após seguir os passos acima o problema continuar, por favor, envie "suporte" para falar diretamente com um atendente.');
+        
     }
 
     if (msg.body !== null && msg.body === '5' && msg.from.endsWith('@c.us')) {
